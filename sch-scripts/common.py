f4ee99241ff188e0f4440a93e44cf9b676adefc8
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2012-09-10 20:03:36 +0300

    Fix greek_to_latin function according iso843.

--- sch-scripts/common.py
+++ sch-scripts/common.py
@@ -31,24 +31,103 @@ def greek_to_latin(name):
     if not isinstance(name, unicode):
         name = name.decode('utf-8')
 
-    mappings = {
-    u'α' : 'a', u'ά' : 'a', u'β' : 'b', u'γ' : 'g', u'δ' : 'd',	u'ε' : 'e',
-    u'έ' : 'e',	u'ζ' : 'z', u'η' : 'i', u'ή' : 'i', u'θ' :'th', u'ι' : 'i',
-    u'ί' : 'i', u'ϊ' : 'i', u'ΐ' : 'i', u'κ' : 'k', u'λ' : 'l', u'μ' : 'm',
-    u'ν' : 'n', u'ξ' : 'x', u'ο' : 'o', u'ό' : 'o', u'π' : 'p', u'ρ' : 'r', 
-    u'σ' : 's', u'ς' : 's', u'τ' : 't', u'υ' : 'i', u'ύ' : 'i', u'ϋ' : 'i', 
-    u'ΰ' : 'i', u'φ' : 'f', u'χ' :'ch', u'ψ' :'ps', u'ω' : 'o', u'ώ' : 'o'}
+    mappings_lowercase = {
+    u'α' : 'a', u'ά' : 'á', u'β' : 'v', u'γ' : 'g', u'δ' : 'd', u'ε' : 'e',
+    u'έ' : 'é', u'ζ' : 'z', u'η' : 'i', u'ή' : 'í', u'θ' :'th', u'ι' : 'i',
+    u'ί' : 'í', u'ϊ' : 'ï', u'ΐ' : 'ḯ', u'κ' : 'k', u'λ' : 'l', u'μ' : 'm',
+    u'ν' : 'n', u'ξ' : 'x', u'ο' : 'o', u'ό' : 'ó', u'π' : 'p', u'ρ' : 'r',
+    u'σ' : 's', u'ς' : 's', u'τ' : 't', u'υ' : 'y', u'ύ' : 'ý', u'ϋ' : 'ÿ',
+    u'ΰ' : 'ÿ́', u'φ' : 'f', u'χ' :'ch', u'ψ' :'ps', u'ω' : 'o', u'ώ' : 'ó',
+    u'γγ':'ng', u'γξ':'nx', u'γχ':'nch'}
 
-    name = name.lower()
-    reg1 = u'(α|ε)(υ|ύ)(β|γ|δ|ζ|λ|μ|ν|ρ|α|ά|ε|έ|η|ή|ι|ϊ|ί|ΐ|ο|ό|υ|ϋ|ύ|ΰ|ω|ώ)'
-    reg2 = u'(α|ε)(υ|ύ)(θ|κ|ξ|π|σ|τ|φ|χ|ψ)'
-    reg3 = u'(ου|ού)'
+    re_reg1 = u'α|ε|η'
+    re_reg2 = u'υ|ύ'
+    re_reg3 = u'β|γ|δ|ζ|λ|μ|ν|ρ|α|ά|ε|έ|η|ή|ι|ί|ϊ|ΐ|ο|ό|υ|ύ|ϋ|ΰ|ω|ώ'
+    re_reg4 = u'θ|κ|ξ|π|σ|τ|φ|χ|ψ'
+    re_reg5 = u'μπ'
 
-    name = re.sub(reg1, u'\\1v\\3', name)
-    name = re.sub(reg2, u'\\1f\\3', name)
-    name = re.sub(reg3, u'ou', name)
+    reg1 = '('+re_reg1.lower()+'|'+re_reg1.upper()+')('+re_reg2.lower()+'|'+re_reg2.upper()+')('+re_reg3.lower()+'|'+re_reg3.upper()+')'
+    
+    reg2 = '('+re_reg1.lower()+'|'+re_reg1.upper()+')('+re_reg2.lower()+'|'+re_reg2.upper()+')('+re_reg4.lower()+'|'+re_reg4.upper()+')'
+    
+    reg3 = u'^μπ|^Μπ|^ΜΠ|^μΠ|μπ$|Μπ$|ΜΠ$|μΠ$'
 
-    return ''.join([mappings[letter] if letter in mappings else letter for letter in name]).encode("utf-8")
+    name = re.sub(reg1, replace_v, name)
+    name = re.sub(reg2, replace_f, name)
+    name = re.sub(reg3, replace_b, name)
+
+    letters = []
+    for letter in name:
+        if letter in mappings_lowercase:
+            letters.append(mappings_lowercase[letter].decode("utf-8"))
+        elif letter.lower() in mappings_lowercase:
+            letters.append((mappings_lowercase[letter.lower()].decode("utf-8")).upper())
+        else:
+            letters.append(letter)
+
+    name = ''.join(letters)
+
+    reg4 = '^PS|^TH|^CH'
+
+    if re.match(reg4, name):
+        name = name.replace(name[1], name[1].lower())
+    
+    return name.encode("utf-8")
+    
+   
+def replace_v(m):
+    if m.group(2).islower():
+        response = m.group(0)
+        if m.group(2) == u'ύ':
+            if m.group(1) == u'α':
+                response = response.replace(m.group(1), u'ά')
+            elif m.group(1) == u'ε':
+                response = response.replace(m.group(1), u'έ')
+            elif m.group(1) == u'η':
+                response = response.replace(m.group(1), u'ή')
+        response = response.replace(m.group(2),u'v')
+        return response
+    else:
+        response = m.group(0)
+        if m.group(2) == u'ύ':
+            if m.group(1) == u'α':
+                response = response.replace(m.group(1), u'ά')
+            elif m.group(1) == u'ε':
+                response = response.replace(m.group(1), u'έ')
+            elif m.group(1) == u'η':
+                response = response.replace(m.group(1), u'ή')
+        response = response.replace(m.group(2),u'V')
+        return response
+
+def replace_f(m):
+    if m.group(2).islower():
+        response = m.group(0)
+        if m.group(2) == u'ύ':
+            if m.group(1) == u'α':
+                response = response.replace(m.group(1), u'ά')
+            elif m.group(1) == u'ε':
+                response = response.replace(m.group(1), u'έ')
+            elif m.group(1) == u'η':
+                response = response.replace(m.group(1), u'ή')
+        response = response.replace(m.group(2),u'f')
+        return response
+    else:
+        response = m.group(0)
+        if m.group(2) == u'ύ':
+           if m.group(1) == u'α':
+                response = response.replace(m.group(1), u'ά')
+           elif m.group(1) == u'ε':
+                response = response.replace(m.group(1), u'έ')
+           elif m.group(1) == u'η':
+                response = response.replace(m.group(1), u'ή')
+        response = response.replace(m.group(2),u'F')
+        return response
+
+def replace_b(m):
+    if m.group(0)[0].islower():
+        return u'b'
+    else:
+        return u'B'
     
 def days_since_epoch():
     epoch = datetime.datetime.utcfromtimestamp(0)
