24d8567ef2d289f440fcad82cc7b35644b738638
Alkis Georgopoulos <alkisg@gmail.com>
2013-12-15 19:13:29 +0200

    Work around apturl not working after firefox 16 (LP: #1261178).

---
+++ share/sch-scripts/sch-scripts.js
@@ -0,0 +1,10 @@
+// Fix apturl
+lockPref("network.protocol-handler.app.apt","/usr/bin/apturl");
+lockPref("network.protocol-handler.warn-external.apt",false);
+lockPref("network.protocol-handler.app.apt+http","/usr/bin/apturl");
+lockPref("network.protocol-handler.warn-external.apt+http",false);
+lockPref("network.protocol-handler.external.apt",true);
+lockPref("network.protocol-handler.external.apt+http",true);
+
+// Disable internal PDF viewer
+lockPref("pdfjs.disabled",true);
