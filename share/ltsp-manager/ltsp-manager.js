540353750371c9526e99825f5388a54caad0ecc1
Franz Dietrich <dietrich@teilgedanken.de>
2018-08-11 09:13:30 +0200

    ltsp-manager.js: change lockPref to pref (sch-scripts LP: #1782789)

--- share/ltsp-manager/ltsp-manager.js
+++ share/ltsp-manager/ltsp-manager.js
@@ -1,13 +1,5 @@
-// Fix apturl
-lockPref("network.protocol-handler.app.apt","/usr/bin/apturl");
-lockPref("network.protocol-handler.warn-external.apt",false);
-lockPref("network.protocol-handler.app.apt+http","/usr/bin/apturl");
-lockPref("network.protocol-handler.warn-external.apt+http",false);
-lockPref("network.protocol-handler.external.apt",true);
-lockPref("network.protocol-handler.external.apt+http",true);
-
 // Disable internal PDF viewer
-lockPref("pdfjs.disabled",true);
+pref("pdfjs.disabled",true);
 
 // Enable flash on file:// URLs
-lockPref("plugins.http_https_only",false);
+pref("plugins.http_https_only",false);
