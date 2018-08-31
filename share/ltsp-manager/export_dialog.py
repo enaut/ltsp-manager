37521d68e326656efa442e65de4c81a5ce84c666
Franz Dietrich <dietrich@teilgedanken.de>
2018-08-31 15:26:18 +0200

    making ltsp-logo a relative path

--- share/ltsp-manager/export_dialog.py
+++ share/ltsp-manager/export_dialog.py
@@ -17,7 +17,7 @@ class ExportDialog:
                                         buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
                                                  Gtk.STOCK_OK, Gtk.ResponseType.OK))
 
-        chooser.set_icon_from_file('/usr/share/pixmaps/ltsp-manager.svg') 
+        chooser.set_icon_from_file('../pixmaps/ltsp-manager.svg')
         chooser.set_default_response(Gtk.ResponseType.OK)
         chooser.set_do_overwrite_confirmation(True)
         homepath = os.path.expanduser('~')
