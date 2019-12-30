2c555a816e9b8f6f33f76e1632c6bc32b1207d1e
Dietrich <dietrich@teilgedanken.de>
2019-12-30 20:07:52 +0100

    Add stdin input capability to the run_command function

--- src/common.py
+++ src/common.py
@@ -19,7 +19,7 @@ except:
     gettext.install('ltsp-manager')
 locale.textdomain('ltsp-manager')
 
-def run_command(cmd, poll=False):
+def run_command(cmd, pw_stdin="", poll=False):
     """ Runs a command and returns either True, on successful
         completion, or the whole stdout and stderr of the command, on error.
         If poll is set return only the process"""
@@ -27,21 +27,23 @@ def run_command(cmd, poll=False):
     # Popen doesn't like integers like uid or gid in the command line.
     cmdline = [str(s) for s in cmd]
 
-
-    p = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     if not poll:
+        pw_out, pw_err = p.communicate(pw_stdin)
+        pw_out = pw_out.decode(sys.stdout.encoding)
+        pw_err = pw_err.decode(sys.stdout.encoding)
+        print(pw_out, pw_err)
         res = p.wait()
         if res == 0:
-            return True, p.stdout.read().decode('utf-8')
+            return True, pw_out
         else:
-            sys.stderr.write("Error while executing command:\n" +
-                " $ %s" % ' '.join(cmdline) + "\n")
-            sys.stderr.write(p.stdout.read().decode('utf-8'))
-            err = p.stderr.read().decode('utf-8')
-            sys.stderr.write(err)
-            if err == '':
-                err = "\n"
-            return False, err
+            sys.stderr.write("Error while executing command:\n"
+                             + " $ %s" % ' '.join(cmdline) + "\n")
+            sys.stderr.write(pw_out)
+            sys.stderr.write(pw_err)
+            if pw_err == '':
+                pw_err = "\n"
+            return False, pw_err
     else:
         return p
 
